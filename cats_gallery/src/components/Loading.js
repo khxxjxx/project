5166db2bcd04e8330b582ca873405a0f11a8fa62
Heejin <khxxjxx@naver.com>
2021-09-04 02:05:33 +0900

    Loading 구현

---
+++ cats_gallery/src/components/Loading.js
@@ -0,0 +1,20 @@
+export default class Loading {
+  constructor({ $app, initialState }) {
+    this.state = initialState;
+    this.$target = document.createElement('div');
+    this.$target.className = 'Modal Loading';
+    $app.appendChild(this.$target);
+  }
+
+  setState(nextState) {
+    this.state = nextState;
+    this.render();
+  }
+
+  render() {
+    this.$target.innerHTML = this.state
+      ? `<div class="content"><img src="./assets/nyan-cat.gif"></div>`
+      : '';
+    this.$target.style.display = this.state ? 'block' : 'none';
+  }
+}
