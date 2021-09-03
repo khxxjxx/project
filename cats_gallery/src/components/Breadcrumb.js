ecb91279e1db31881dadf95f76cec2eb32218b20
Heejin <khxxjxx@naver.com>
2021-09-04 02:17:57 +0900

    Nav Click 구현

--- cats_gallery/src/components/Breadcrumb.js
+++ cats_gallery/src/components/Breadcrumb.js
@@ -1,9 +1,22 @@
 export default class Breadcrumb {
-  constructor({ $app, initialState }) {
+  constructor({ $app, initialState, onNavClick }) {
     this.state = initialState;
+    this.onNavClick = onNavClick;
     this.$target = document.createElement('nav');
     this.$target.className = 'Breadcrumb';
     $app.appendChild(this.$target);
+
+    this.$target.addEventListener('click', e => {
+      const nav = e.target.closest('.nav');
+      if (nav) {
+        const { index } = nav.dataset;
+        if (!index) {
+          this.onNavClick();
+        } else {
+          this.onNavClick(index);
+        }
+      }
+    });
   }
 
   setState(nextState) {
@@ -13,8 +26,15 @@ export default class Breadcrumb {
 
   render() {
     const isRoot = this.state.length === 0;
-    this.$target.innerHTML = `<div>root</div>${
-      isRoot ? '' : this.state.map(depth => `<div>${depth.name}</div>`).join('')
+    this.$target.innerHTML = `<div class="nav">root</div>${
+      isRoot
+        ? ''
+        : this.state
+            .map(
+              (depth, idx) =>
+                `<div class="nav" data-index=${idx}>${depth.name}</div>`
+            )
+            .join('')
     }`;
   }
 }
